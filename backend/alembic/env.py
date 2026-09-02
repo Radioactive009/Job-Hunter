c8f5a64cde986ec7e9a5fb93b1ade1a58b893f9f
Kislay Kumar <kislayreigns20092002@gmail.com>
2026-09-02 22:33:56 +0530

    initial:

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -2,7 +2,8 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
+from app.database import Base
+from app.models import *
 from alembic import context
 
 # this is the Alembic Config object, which provides
