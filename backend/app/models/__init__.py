5bf2b1db9e5dc098fdde2c8b2ef68a08987f6840
Kislay Kumar <kislayreigns20092002@gmail.com>
2026-09-02 22:54:48 +0530

    initial:

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,6 @@
 from app.models.user import User
 from app.models.profile import Profile
-from app.models.skills import skills
+from app.models.skills import Skill
 from app.models.profile_skill import ProfileSkill
 from app.models.company import Company
 from app.models.job import Job
